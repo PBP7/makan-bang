a10c1e18669edf65ee7236433d540ae45ca3ead8
TapTaps7 <ghathafabiyyu@gmail.com>
2024-10-24 13:06:26 +0700

    Update settings

--- makan_bang/settings.py
+++ makan_bang/settings.py
@@ -39,7 +39,10 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
     'main',
     'authentication',
-    'katalog'
+    'rateNreview',
+    'preference',
+    'katalog',
+    'bookmark',
 ]
 
 MIDDLEWARE = [
