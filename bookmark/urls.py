c5b999cfb60089f53082e403a51cfc687cf03629
TapTaps7 <ghathafabiyyu@gmail.com>
2024-10-26 17:49:57 +0700

    Tinggal desain

--- bookmark/urls.py
+++ bookmark/urls.py
@@ -1,10 +1,10 @@
 from django.urls import path
-from . import views
+from bookmark.views import*
 
 app_name = 'bookmark'
 
 urlpatterns = [
-    path('', views.bookmark_list, name='bookmark_list'),
-    path('bookmarks/remove/<int:product_id>/', views.remove_bookmark, name='remove_bookmark'),
-    path('bookmarks/button/<int:product_id>/', views.toggle_bookmark, name='toggle_bookmark'),
+    path('', bookmark_list, name='bookmark_list'),
+    path('remove/<uuid:product_id>/', remove_bookmark, name='remove_bookmark'),
+    path('toggle_bookmark/<uuid:product_id>/', toggle_bookmark, name='toggle_bookmark'),
 ]
