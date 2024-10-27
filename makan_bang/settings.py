cdd12228e79aed8f4eb35afcea420b7ef2402ab7
izycoder <farizmuhammad39@gmail.com>
2024-10-27 22:29:01 +0700

    pws

--- makan_bang/settings.py
+++ makan_bang/settings.py
@@ -30,7 +30,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-rktgn0o^am*x97_x28-7arg=(97ff9!*e_p&ahm^6oblex-xn&'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = ["localhost", "127.0.0.1", "fariz-muhammad31-makanbang.pbp.cs.ui.ac.id"]
 
