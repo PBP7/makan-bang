ef8f16c158865829a218dec3cfa93f6012e2ca7c
izycoder <farizmuhammad39@gmail.com>
2024-10-27 22:37:36 +0700

    pws gambar

--- makan_bang/settings.py
+++ makan_bang/settings.py
@@ -57,6 +57,7 @@ INSTALLED_APPS = [
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
+    'whitenoise.middleware.WhiteNoiseMiddleware', # WHITE NOISE
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
